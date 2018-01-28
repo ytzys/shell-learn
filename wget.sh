608028eae719ad9a1580d1970461cba632603ccb
临江仙 <ytzys@mail.ustc.edu.cn>
2018-01-28 14:08:28 +0800

    Update wget.sh

--- wget.sh
+++ wget.sh
@@ -1,3 +1,16 @@
 #/bin/bash
-
+# how to print the web cotent to the console without donwload it.
 wget -q -O - http://www.baidu.com/
+
+# with post data
+wget -q -O - http://www.baidu.com/ --post-data=imgContinue=Continue+to+image+...+
+
+# or you can use curl
+curl http://www.baidu.com/ -d imgContinue=Continue+to+image+...+
+
+# grep something, sometimes you have to grep two times
+grep -o "http://www.baidu.com/.*.jpg' title" | grep -o http.*jpg
+
+
+
+
